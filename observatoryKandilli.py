356465146ddf23e51b9bd74183a86e07e073ec92
emrahburak <gemrahburak@gmail.com>
2020-06-09 20:42:48 +0300

    refactoring - 0.0.1

--- observatoryKandilli.py
+++ observatoryKandilli.py
@@ -26,7 +26,7 @@ try:
     import json
     import csv
     import re
-    import numpy as np
+    # import numpy as np
     from bs4 import BeautifulSoup
     from datetime import datetime as dt
 except Exception as err:
@@ -52,10 +52,10 @@ class GetBase():
         self._error = True
         self.float_base = 0.0
         self.float_oldbase = ['-.-']
-        self.cols = ['Tarih', 'Saat', 'Enlem',
-                     'Boylam', 'DerinlikKm',
-                     'MD', 'ML', 'Mw', 'Yer',
-                     'ÇozumNiteligi']
+        self.cols = ('tarih', 'saat', 'enlem',
+                     'boylam', 'derinlikkm',
+                     'md', 'ml', 'mw', 'yer',
+                     'cozumniteligi')
 
         # specify the URL of the archive here ***
         self.archive_url = "http://www.koeri.boun.edu.tr/scripts/lst2.asp"
@@ -77,7 +77,7 @@ class GetLink(GetBase):
             # soup = BeautifulSoup(r.content, 'html.parser')
 
             # find all links on web-page
-            self.text = self.soup.get_text()
+            # self.text = self.soup.get_text()
             self.pre = self.soup.find_all('pre')[-1]
             self.get_text = self.pre.text
 
@@ -93,44 +93,80 @@ class GetLink(GetBase):
         finally:
             if self._error:
                 return self.link_parsing()
+                # return self.get_data
 
     def link_parsing(self):
         try:
             for num_line, line in enumerate(self.get_data.splitlines()):
                 self.dict = {}
                 self.counter_numline.append(num_line)
-                self.matrix_base = np.array(line.split())
+                self.matrix_base = line.split()
+                # self.list_base = line.split()
+                # assert(self.matrix_base == self.list_base, "Get A problem")
+
+                
                 for dict_index in range(len(self.cols[:5])):
                     self.dict[self.cols[dict_index]] = self.matrix_base[dict_index]
+
+
+
                 if self.matrix_base[5] in self.float_oldbase:
                     self.dict[self.cols[5]] = self.float_base
+
                 else:
                     self.dict[self.cols[5]] = self.matrix_base[5]
+
                 if self.matrix_base[6] in self.float_oldbase:
                     self.dict[self.cols[6]] = self.float_base
+
                 else:
                     self.dict[self.cols[6]] = self.matrix_base[6]
+
                 if self.matrix_base[7] in self.float_oldbase:
                     self.dict[self.cols[7]] = self.float_base
+
                 else:
                     self.dict[self.cols[7]] = self.matrix_base[7]
+
 
                 self.matrix_col_eight = [x for x in self.matrix_base[8:] if x.isupper()]
                 self.matrix_revize = [y for y in self.matrix_base[8:] if re.search("[A-Z]+[0-9]+", y)]
                 self.matrix_revize_date = [p for p in self.matrix_base[8:] if re.search(self.dateregex, p)]
                 self.matrix_revize_time = [k for k in self.matrix_base[8:] if re.match(self.timeregex, k)]
-                self.matrix_ilksel = np.array(
-                    [z for z in self.matrix_base[8:] if z in self.matrix_base[-1] and re.match('^.*[a-z]', z)])
+                self.list_ilksel = [z for z in self.matrix_base[8:] if z in self.matrix_base[-1] and re.match('^.*[a-z]', z)]
+                # self.matrix_ilksel = np.array(
+                #     [z for z in self.matrix_base[8:] if z in self.matrix_base[-1] and re.match('^.*[a-z]', z)])
+                self.matrix_ilksel = self.list_ilksel
+
+
+                # print(self.list_ilksel)
+
+
+
+                
                 if self.matrix_col_eight:
                     self.dict[self.cols[8]] = ''.join(map(str, self.matrix_col_eight))
 
                 if self.matrix_revize or self.matrix_revize_date or self.matrix_revize_time:
                     self.dict[self.cols[9]] = self.matrix_revize[0] + '' + self.matrix_revize_date[0] + '-' + \
-                                              self.matrix_revize_time[0]
-                if self.matrix_ilksel:
+                        self.matrix_revize_time[0]
+
+                # if self.matrix_ilksel.size > 0:
+                if len(self.matrix_ilksel) > 0:
                     self.dict[self.cols[9]] = self.matrix_ilksel[0]
 
+
+                # # convert datetime form 
+                # self.dict['tarih'] = datetime_converter(self.dict['tarih'])
                 self.dict_box.append(self.dict)
+
+
+
+
+
+
+
+            
         except (Exception) as error:
             print(error)
             self._error = False
@@ -138,12 +174,16 @@ class GetLink(GetBase):
         finally:
             if self._error:
                 if self.dict_box:
-                    print(self.dict_box)
+                    # print(self.dict_box)
+
                     return self.dict_box
+
+
 
                     # +++++++Check your data+++++++
                     # for num, i in enumerate(self.dict_box):
                     #     print(num, i)
+
 
 
 
